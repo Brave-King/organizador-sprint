896cea7dbf23aea7781e9c2be0aa1f3da0c01c4b
Brave-King <ramiro.vargas@udgvirtual.udg.mx>
2023-03-15 19:15:29 -0600

    nuevos cambios en archivo

--- src/App.js
+++ src/App.js
@@ -5,99 +5,211 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       name: "Preparación de material",
-      startDate: "02/08/2023",
-      endDate: "02/09/2023",
+      startDate: "02/20/2023",
+      endDate: "02/20/2023",
       priority: "Media",
       color: "orange"
     },
     {
       name: "Junta de equipo",
-      startDate: "02/10/2023",
-      endDate: "02/10/2023",
+      startDate: "02/20/2023",
+      endDate: "02/20/2023",
       priority: "Media",
       color: "orange"
     },
     {
       name: "Junta con el cliente",
-      startDate: "02/11/2023",
-      endDate: "02/11/2023",
+      startDate: "02/20/2023",
+      endDate: "02/20/2023",
       priority: "Alta",
       color: "red"
     },
     {
       name: "Lluvia de ideas rebranding",
-      startDate: "02/13/2023",
-      endDate: "02/13/2023",
+      startDate: "02/21/2023",
+      endDate: "02/21/2023",
       priority: "Baja",
       color: "green"
     },
     {
       name: "Bosquejo UX y detallado paleta de colores",
-      startDate: "02/13/2023",
-      endDate: "02/13/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Bosquejado de versiones de Home Page",
-      startDate: "02/14/2023",
-      endDate: "02/14/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Bosquejo global del sitio",
-      startDate: "02/14/2023",
-      endDate: "02/14/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Junta equipo coding",
-      startDate: "02/15/2023",
-      endDate: "02/15/2023",
-      priority: "Baja",
-      color: "green"
-    },
-    {
-      name: "Bosquejo global del sitio",
-      startDate: "02/15/2023",
-      endDate: "02/16/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Test alfa del modelo de base de datos",
-      startDate: "02/17/2023",
-      endDate: "02/18/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Diseño de headers, tipografías y espacios publicitarios",
-      startDate: "02/19/2023",
-      endDate: "02/20/2023",
-      priority: "Baja",
-      color: "green"
-    },
-    {
-      name: "Diseño de Home Page, menú de opciones",
-      startDate: "02/20/2023",
-      endDate: "02/21/2023",
-      priority: "Media",
-      color: "orange"
-    },
-    {
-      name: "Diseño de login, UX de perfiles de usuarios",
       startDate: "02/22/2023",
       endDate: "02/22/2023",
       priority: "Media",
       color: "orange"
     },
     {
-      name: "Diseño de paginas secundarias y newsletter",
+      name: "Bosquejado de versiones de Home Page",
       startDate: "02/23/2023",
       endDate: "02/23/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Bosquejo global del sitio",
+      startDate: "02/24/2023",
+      endDate: "02/24/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Bosquejado de versiones de home page",
+      startDate: "02/27/2023",
+      endDate: "02/27/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Junta Equipo Coding",
+      startDate: "02/28/2023",
+      endDate: "02/28/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Bosquejo global del mapa sitio",
+      startDate: "02/28/2023",
+      endDate: "02/28/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Modelado principal de la base de datos",
+      startDate: "02/28/2023",
+      endDate: "03/01/2023",
+      priority: "Alta",
+      color: "red"
+    },
+    {
+      name: "Test alfa del modelo de base de datos",
+      startDate: "03/01/2023",
+      endDate: "03/02/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño de headers, tipografías y espacios publicitarios",
+      startDate: "03/03/2023",
+      endDate: "03/03/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Diseño de Home Page, menú de opciones",
+      startDate: "03/06/2023",
+      endDate: "03/06/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño de login, UX de perfiles de usuarios",
+      startDate: "03/06/2023",
+      endDate: "03/06/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño de paginas secundarias y newsletter",
+      startDate: "03/07/2023",
+      endDate: "03/07/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Test base de datos",
+      startDate: "03/07/2023",
+      endDate: "03/07/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño Final de la Homepage",
+      startDate: "03/08/2023",
+      endDate: "03/08/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño final de la UX",
+      startDate: "03/08/2023",
+      endDate: "03/08/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño final newsletter",
+      startDate: "03/08/2023",
+      endDate: "03/08/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Diseño de headers, tipografías y espacios publicitarios",
+      startDate: "03/13/2023",
+      endDate: "03/13/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Diseño de home page, menú de opciones",
+      startDate: "03/13/2023",
+      endDate: "03/13/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Diseño de login, UX de perfiles de usuarios",
+      startDate: "03/13/2023",
+      endDate: "03/13/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Test base de datos",
+      startDate: "03/14/2023",
+      endDate: "03/14/2023",
+      priority: "Media",
+      color:" orange"
+    },
+    {
+      name: "Diseño final de la Homepage",
+      startDate: "03/15/2023",
+      endDate: "03/15/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Diseño final de UX",
+      startDate: "03/15/2023",
+      endDate: "03/15/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Diseño final newsletter",
+      startDate: "03/15/2023",
+      endDate: "03/15/2023",
+      priority: "Baja",
+      color: "green"
+    },
+    {
+      name: "Test usabilidad base",
+      startDate: "03/16/2023",
+      endDate: "03/16/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Test seguridad base",
+      startDate: "03/16/2023",
+      endDate: "03/16/2023",
+      priority: "Media",
+      color: "orange"
+    },
+    {
+      name: "Llenado de bitácora",
+      startDate: "03/17/2023",
+      endDate: "03/17/2023",
       priority: "Baja",
       color: "green"
     },
