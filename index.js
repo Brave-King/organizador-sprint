e0d9142778e89583b039db3214fa98e8c6a9bc4e
Martin Pizano M <mpizanom@gmail.com>
2023-03-21 20:12:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import NewsGrid from './components/NewsGrid.js';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+     <Navbar />
+      <NewsGrid />
+      <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+/*
+import React from 'react';
+import './App.css';
+import NewsGrid from './components/NewsGrid';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <NewsGrid />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+*/
