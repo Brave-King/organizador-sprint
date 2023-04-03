a1d468cc822ecb92e31867e14ec404699f1296d6
Martin Pizano M <mpizanom@gmail.com>
2023-04-02 20:36:28 -0600

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="footer">
+    <footer>
       <div className="container">
-        <p>Este es el texto del footer</p>
+        <div className="row">
+          <div className="col-sm-12 col-md-4">
+            <h4>Contáctanos</h4>
+            <ul>
+              <li>Teléfono: 123-456-7890</li>
+              <li>Email: info@noticiasgeeks.com</li>
+              <li>Dirección: 1234 Calle Principal</li>
+            </ul>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <h4>Redes Sociales</h4>
+            <ul>
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <h4>Acerca de Nosotros</h4>
+            
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <p>
+              © 2023 Noticias Geeks. Todos los derechos reservados.
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-6">
+            <ul>
+              <li>Términos y Condiciones</li>
+              <li>Política de Privacidad</li>
+              <li>Política de Cookies</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
